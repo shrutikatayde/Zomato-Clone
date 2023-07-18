336af94fb3f371a99423d892c4519f9f74dbba7c
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-07-18 23:56:08 +0530

    api

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const Router = express.Router();
+import {UserModel} from "../../database/user/index"
 
 /*
 Route     /signup
@@ -10,9 +13,9 @@ Access    Public
 Method    POST
 */
 
-Router.post("/signup", async (req, res) => {
+Router.post("/signup", async (req, res) => { 
     try {
-        const { email, password, fullname, phoneNUmber } = req.body.credentials;
+        const { email, password, fullname, phoneNumber } = req.body.credentials;
 
         //Check whether email or phone number exists
         const checkUserByEmail = await UserModel.findOne({ email });
@@ -21,6 +24,22 @@ Router.post("/signup", async (req, res) => {
         if (checkUserByEmail || checkUserByPhone) {
             return res.json({error: "User already exists"})
         }
+
+        //hashing
+        const bcryptSalt = await bcrypt.genSalt(8);
+        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+
+        //database 
+        await UserModel.create({
+            ...req.body.credentials,
+            password: hashedPassword
+        });
+
+        //JWT Auth Token --> to make transfering data between parties more secure
+        const token = jwt.sign({ user: { fullname, email } }, "ZomatoApp");
+
+        return res.status(200).json({ token });
+
 
     }
     catch (error) {
